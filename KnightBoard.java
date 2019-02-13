be75e73911dd40af196444435ed64f99ebad90d0
Amanda Chen <achen01@stuy.edu>
2019-02-13 10:22:23 -0500

    created instance method

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,7 +1,12 @@
 public class KnightBoard {
+
+  private int[] board;
+
   public KnightBoard(int startingRows,int startingCols){
     if (startingRows < 0 || startingCols < 0){
       throw new IllegalArgumentException("Negative Parameters");
     }
+
   }
+
 }
