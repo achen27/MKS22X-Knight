022b70888c6961775412a59e7c96f1d55819f2ef
Amanda Chen <achen01@stuy.edu>
2019-02-13 10:23:56 -0500

    fixed instance method and changed it in constructor

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,12 +1,13 @@
 public class KnightBoard {
 
-  private int[] board;
+  private int[][] board;
 
   public KnightBoard(int startingRows,int startingCols){
     if (startingRows < 0 || startingCols < 0){
       throw new IllegalArgumentException("Negative Parameters");
     }
-
+    board = new int[startingRows][startingCols];
+    //for (int i = 0; i < startingRows)
   }
 
 }
