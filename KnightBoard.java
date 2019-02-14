8d27d4ddaabfe825531777b72a8cc145c6734a15
Amanda Chen <achen01@stuy.edu>
2019-02-14 10:34:22 -0500

    added exceptions to solve

--- KnightBoard.java
+++ KnightBoard.java
@@ -44,6 +44,16 @@ public class KnightBoard {
   *@return true when the board is solvable from the specified starting position
   */
   public boolean solve(int startingRow, int startingCol){
+    for (int i = 0; i < board.length; i++){
+      for (int j = 0; j < board[0].length; j++){
+        if (board[i][j] != 0){
+          throw new IllegalStateException("board contains non-zero values");
+        }
+      }
+    }
+    if (startingRow < 0 || startingRow > board.length - 1 || startingCol < 0 || startingCol > board.length - 1){
+      throw new IllegalArgumentException();
+    }
     return solveH(startingRow, startingCol, 1);
   }
 
@@ -61,10 +71,15 @@ public class KnightBoard {
       return false;
     }
     board[row][col] = level;
+    System.out.println(this.toString());
     for (int i = -2; i < 3; i++){
       for (int j = -2; j < 3; j++){
         if (i != 0 && j != 0){
-          return solveH(row+i,col+j,level+1);
+          if(solveH(row+i,col+j,level+1)){
+            return true;
+          } else {
+            board[row][col] = 0;
+          }
         }
       }
     }
@@ -73,7 +88,8 @@ public class KnightBoard {
 
   public static void main(String[] args){
     KnightBoard test = new KnightBoard(3,3);
-    System.out.println(test);
+    System.out.println(test.solve(0,0));
+
   }
 
 }
