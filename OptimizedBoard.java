44537dace37882f5a926b68f9c0708b9bc6fc608
Amanda Chen <achen01@stuy.edu>
2019-02-26 15:43:45 -0500

    added comments to object class

--- OptimizedBoard.java
+++ OptimizedBoard.java
@@ -1,23 +1,23 @@
 public class OptimizedBoard{
-  private int moves;
-  private int x;
-  private int y;
+  private int moves; //stores number of outgoing moves
+  private int x; //stores x coordinate
+  private int y; //stores y coordinate
 
-  public OptimizedBoard(int[][] movesBoard, int row, int col){
+  public OptimizedBoard(int[][] movesBoard, int row, int col){ //constructs new object and instantiates fields
     moves = movesBoard[row][col];
     x = row;
     y = col;
   }
 
-  public int getMoves(){
+  public int getMoves(){ //returns outgoing moves
     return moves;
   }
 
-  public int getX(){
+  public int getX(){ //returns x coordinate
     return x;
   }
 
-  public int getY(){
+  public int getY(){ //returns y coordinate
     return y;
   }
 }
