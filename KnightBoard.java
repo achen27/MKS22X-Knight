de57534015abd26be2e336262520d7bda8587930
Amanda Chen <achen01@stuy.edu>
2019-02-26 11:56:01 -0500

    created two arrays to store knight moves

--- KnightBoard.java
+++ KnightBoard.java
@@ -2,8 +2,28 @@ public class KnightBoard {
 
   private int[][] board;
   private int[][] moves;
+  private int[] x;
+  private int[] y;
 
   public KnightBoard(int startingRows,int startingCols){
+    x = new int[8];
+    y = new int[8];
+    x[0] = 1;
+    y[0] = 2;
+    x[1] = 1;
+    y[1] = -2;
+    x[2] = 2;
+    y[2] = 1;
+    x[3] = 2;
+    y[3] = -1;
+    x[4] = -1;
+    y[4] = 2;
+    x[5] = -1;
+    y[5] = -2;
+    x[6] = -2;
+    y[6] = 1;
+    x[7] = -2;
+    y[7] = -1;
     if (startingRows <= 0 || startingCols <= 0){
       throw new IllegalArgumentException("Negative Parameters");
     }
