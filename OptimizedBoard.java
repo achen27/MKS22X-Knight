7acbb9f4a8b5b7fd52e1755cba056f0751affffd
Amanda Chen <achen01@stuy.edu>
2019-02-26 11:54:39 -0500

    wrote get methods

--- OptimizedBoard.java
+++ OptimizedBoard.java
@@ -6,10 +6,21 @@ public class OptimizedBoard{
   private int x;
   private int y;
 
-
   public OptimizedBoard(int[][] movesBoard, int row, int col){
     moves = movesBoard[row][col];
     x = row;
     y = col;
+  }
+
+  public int getMoves(){
+    return moves;
+  }
+
+  public int getX(){
+    return x;
+  }
+
+  public int getY(){
+    return y;
   }
 }
